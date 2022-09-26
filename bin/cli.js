460372508186ec1f68a91edd60f51bcd9f69ee0e
Yannick Maes <yannick.maes@nipro-group.com>
2022-09-26 09:39:25 +0200

    chore: increase prettier print width

--- bin/cli.js
+++ bin/cli.js
@@ -12,28 +12,21 @@ function main() {
   // Arguments
 
   const args = process.argv.slice(2);
-  const checkDuplicates =
-    args.includes('--duplicate-keys') || args.includes('-dk');
+  const checkDuplicates = args.includes('--duplicate-keys') || args.includes('-dk');
 
   const saveLog = args.includes('--save-log') || args.includes('-sl');
 
-  const filesArg = args.find(
-    (a) => a.includes('--files-path') || a.includes('-fp')
-  );
+  const filesArg = args.find((a) => a.includes('--files-path') || a.includes('-fp'));
   const filesPath = filesArg
     ? process.cwd() + `/${filesArg.split('=')[1]}`
     : process.cwd() + '/src/**/*.{ts,tsx,js,jsx}';
 
-  const translationArg = args.find(
-    (a) => a.includes('--translation-file') || a.includes('-tf')
-  );
+  const translationArg = args.find((a) => a.includes('--translation-file') || a.includes('-tf'));
   const translationPath = translationArg
     ? process.cwd() + `/${translationArg.split('=')[1]}`
     : process.cwd() + '/src/translations/en.json';
 
-  const oldTranslationPathArg = args.find(
-    (a) => a.includes('--old-translation-file') || a.includes('-otf')
-  );
+  const oldTranslationPathArg = args.find((a) => a.includes('--old-translation-file') || a.includes('-otf'));
 
   const oldTranslationPath = oldTranslationPathArg
     ? process.cwd() + `/${oldTranslationPathArg.split('=')[1]}`
